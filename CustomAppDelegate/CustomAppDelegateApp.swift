549792b06c47d4a75d7487fc60c8d3cb52debc45
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-11 18:10:31 -0300

    Initial Commit

---
+++ CustomAppDelegate/CustomAppDelegateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAppDelegateApp.swift
+//  CustomAppDelegate
+//
+//  Created by Jonn Alves on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomAppDelegateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
